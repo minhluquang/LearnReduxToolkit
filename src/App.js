dc5478459a622dc64ca3d1aee9593ed6edb1fc8a
minhluquang <luminh2004@gmail.com>
2023-12-24 23:01:45 +0700

    25. Khởi tạo React với Redux Toolkit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/slices/counterSlide";
 
 function App() {
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.counter.value);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world React with Hoi Dan IT
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello world React with LQM</p>
+        <div>
+          <button onClick={() => dispatch(increment())}>Increment</button>
+          <button onClick={() => dispatch(decrement())}>Decrement</button>
+          <div>Count = {count}</div>
+        </div>
       </header>
     </div>
   );
